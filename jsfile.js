e46c40082be534a99ab0c141917f2b0aaf9632d4
Thet Paing Lin <thatpainlin1999@gmail.com>
2024-10-17 16:53:38 +0000

    Style cards & validate from with js

--- jsfile.js
+++ jsfile.js
@@ -3,22 +3,28 @@ const cardNumber = document.querySelector("#cardNumbers");
 const cardholderName = document.querySelector("#cardholderName");
 const expiredDate = document.querySelector("#expiredDate");
 const cvcNumbers = document.querySelector("#cvcNumbers");
+const form = document.querySelector('#dataForm');
+
+const errorMessage = document.querySelector("#error-message");
 
 // Select the button
 const submitButton = document.querySelector("#submit");
 
 submitButton.addEventListener('click',(event)=>{
     event.preventDefault();
-
     const numbers = document.getElementById('cardnumbers').value;
-    cardNumber.textContent = numbers;
-    
     const name = document.getElementById('cardholder-name').value;
-    cardholderName.textContent = name;
-
     const expdate = document.getElementById('expDate').value;
-    expiredDate.textContent = expdate;
-
     const cvc = document.getElementById('cvc').value;
-    cvcNumbers.textContent = cvc;
+
+    if(numbers === '' || name === '' || expdate === '' || cvc === ''){
+        errorMessage.textContent = 'Please fill the all the field correctly.';
+    }else if(!form.checkValidity()){
+        errorMessage.textContent = 'Please fill the all the field correctly.';
+    }else{
+        cardNumber.textContent = numbers;
+        cardholderName.textContent = name;
+        expiredDate.textContent = expdate;
+        cvcNumbers.textContent = cvc;
+    }
 });
